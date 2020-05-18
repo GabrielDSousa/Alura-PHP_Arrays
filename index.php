2e7e202554c70796d742786abe724ebe03b8c764
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2020-05-18 14:54:56 -0300

    Second lesson

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require 'Calculator.php';
 $report = [6,7,8,7,8,5,9];
 $calc = new Calculator();
 $average = $calc->average($report);
+$names = "Gabriel, Ana, Clara, Igor, Bernardo";
+$names_array = explode(', ',$names);
+$report_imploded = implode(', ',$report);
 ?>
 
 <!doctype html>
@@ -18,5 +21,16 @@ $average = $calc->average($report);
     <h1>Php Arrays</h1>
     <h2>First lesson, create a school report average calculator:</h2>
     <p>The <b>average</b> of the school report is: <?=$average?></p>
+    <h2>Second lesson, explode and implode:</h2>
+    <h3>Explode</h3>
+    <h4>A string of name exploded as array, and iterated</h4>
+    <ul>
+        <?php foreach ($names_array as $name){?>
+            <li>Hi,<?=$name?></li>
+        <?php } ?>
+    </ul>
+    <h3>Implode</h3>
+    <p>School report imploded in a string: <?=$report_imploded?></p>
+
 </body>
 </html>
