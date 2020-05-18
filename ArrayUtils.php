bdd2c8de6ce29970c22796399a6db682cfc66df2
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2020-05-18 15:43:47 -0300

    Third lesson

---
+++ ArrayUtils.php
@@ -0,0 +1,16 @@
+<?php
+
+class ArrayUtils
+{
+    public static function remove($element, array &$array): array
+    {
+        $position = array_search($element, $array, true);
+
+        if (is_int($position)) {
+            unset($array[$position]);
+            return $array;
+        }
+
+        return $array;
+    }
+}
--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 require 'Calculator.php';
+require 'ArrayUtils.php';
+
+//first lesson
 $report = [6,7,8,7,8,5,9];
 $calc = new Calculator();
 $average = $calc->average($report);
+
+//second lesson
 $names = "Gabriel, Ana, Clara, Igor, Bernardo";
 $names_array = explode(', ',$names);
 $report_imploded = implode(', ',$report);
+
+//third lesson
+$psychotic_array = ["Giovanni", "John", 12, "Maria", 25, "Luis", "Luisa", "12", "Rafael"];
+$psychotic_array = ArrayUtils::remove("Giovanni", $psychotic_array);
 ?>
 
 <!doctype html>
@@ -21,8 +30,10 @@ $report_imploded = implode(', ',$report);
     <h1>Php Arrays</h1>
     <h2>First lesson, create a school report average calculator:</h2>
     <p>The <b>average</b> of the school report is: <?=$average?></p>
+    <br><br>
     <h2>Second lesson, explode and implode:</h2>
     <h3>Explode</h3>
+    <br>
     <h4>A string of name exploded as array, and iterated</h4>
     <ul>
         <?php foreach ($names_array as $name){?>
@@ -30,7 +41,39 @@ $report_imploded = implode(', ',$report);
         <?php } ?>
     </ul>
     <h3>Implode</h3>
+    <br>
     <p>School report imploded in a string: <?=$report_imploded?></p>
+    <br><br>
+    <h3>Third lesson, removing a element  of a array</h3>
+    <p>The array is a psychotic thing like: ["Giovanni", "John", 12, "Maria", 25, "Luis", "Luisa", "12", "Rafael"]</p>
+    <p>Removing the "Giovani"</p>
+    <p>
+        <?php
+        foreach ($psychotic_array as $item){
+            echo "$item, ";
+        }
+        ?>
+    </p>
+
+    <p>Removing the "12", not the 12</p>
+    <p>
+        <?php
+        $psychotic_array = ArrayUtils::remove("12", $psychotic_array);
+        foreach ($psychotic_array as $item){
+            echo "$item, ";
+        }
+        ?>
+    </p>
+
+    <p>Removing the 12, not the "12"</p>
+    <p>
+        <?php
+        $psychotic_array = ArrayUtils::remove(12, $psychotic_array);
+        foreach ($psychotic_array as $item){
+            echo "$item, ";
+        }
+        ?>
+    </p>
 
 </body>
 </html>
